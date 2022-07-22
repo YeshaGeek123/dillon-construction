f40d5b4b9e2c7c7213d7df28662e983aeed01d50
YeshaGeek123 <yeshageek435@gmail.com>
2022-07-22 11:16:12 +0530

    22-7-22

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * dillon-construction Theme Customizer
+ *
+ * @package dillon-construction
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function dillon_construction_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'dillon_construction_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'dillon_construction_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'dillon_construction_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function dillon_construction_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function dillon_construction_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function dillon_construction_customize_preview_js() {
+	wp_enqueue_script( 'dillon-construction-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'dillon_construction_customize_preview_js' );
